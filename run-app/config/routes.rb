c5a58f10887dd64b937a8ad980139eb6907d8f24
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-17 05:43:50 +0000

    Se agregó la gema will_paginate en GemFile y se instaló

--- run-app/config/routes.rb
+++ run-app/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
-  get 'home/recuperacion_contrasena' => 'home#recuperacion_contrasena'
-
-  get 'home/registro'
-
   root to: 'home#index'
+  
+  get 'home/recuperacion_contrasena' => 'home#recuperacion_contrasena'
+  get 'home/registro'
   get '/home/entrar'
+  get 'users/show'
   
   devise_for :users, :path => 'home', :path_names => { :sign_in => 'entrar', :sign_up => 'registro', :sign_out => 'logout', :password => 'recuperacion_contrasena', :confirmation => 'verificación' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
