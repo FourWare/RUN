6a25c926aa4b9d41b0f7bb5f2dd6e6d02f1169d7
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-20 03:58:40 +0000

    Agregando nuevos atributos a usuario

--- run-app/app/mailers/user_mailer.rb
+++ run-app/app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
     def welcome_email(user)
         @user = user
+        @name = user.name
         @url = 'https://run-app-miguelangelborja-1.c9users.io/home/entrar'
         #Adjuntar documentos:  attachments["backgroundEmail.jpg"] = File.read("#{Rails.root}/app/assets/images/backgroundEmail.jpg")
         mail(to: user.email, subject: 'Bienvenido a RunApp', from: 'runapp@mysite.com')
