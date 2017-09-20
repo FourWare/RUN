44b39a47c67286963dd102f9257d6267aaa4488d
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-20 05:25:33 +0000

    Corrección de errores en los nuevos atributos de usuario al registrarse

--- run-app/app/controllers/application_controller.rb
+++ run-app/app/controllers/application_controller.rb
@@ -4,10 +4,11 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+    protected
 
     def configure_permitted_parameters
-      registration_params = [:name, :email, :password, :password_confirmation]
+      devise_parameter_sanitizer.permit(:sign_up,        keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
 
 end
