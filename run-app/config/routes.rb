600276191aa3d6d2437aea7585ae55702372bad3
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-22 07:19:31 +0000

    Implementación de la gema omniauth. Permite loguearse con Facebook.

--- run-app/config/routes.rb
+++ run-app/config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   get 'home/recuperacion_contrasena' => 'home#recuperacion_contrasena'
   get 'home/registro'
   get '/home/entrar'
-
+  
   root to: 'home#index'
   
-  devise_for :users, :path => 'home', :path_names => { :sign_in => 'entrar', :sign_up => 'registro', :sign_out => 'logout', :password => 'recuperacion_contrasena', :confirmation => 'verificación' }
+  devise_for :users, :path => 'home', :path_names => { :sign_in => 'entrar', :sign_up => 'registro', :sign_out => 'logout', :password => 'recuperacion_contrasena', :confirmation => 'verificación' },
+            :controllers => { omniauth_callbacks: "sessions" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
 end
