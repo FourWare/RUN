120560f9b94583eb1164323cd91d6e46830772cb
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-25 00:12:20 +0000

    Configuración en la vista de administrador - Se puede acceder cuando se inicie sesión

--- run-app/app/controllers/admin_controller.rb
+++ run-app/app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
+  before_action :authenticate_user!
+  
   def home
     @users = User.paginate(page: params[:page], per_page: 1)
     render :layout => 'admin_layout'
