5eba22f69a06a21eca0e50ebb1c469463cf21994
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-27 18:36:17 +0000

    Implementación inicial del faker.

--- run-app/db/seeds.rb
+++ run-app/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+    user = User.new
+    
+    user.assign_attributes({ 
+        :name => Faker::Name.name, 
+        :nick => Faker::Name.first_name, 
+        :career => Faker::University.name, 
+        :email => Faker::Name.first_name + '@unal.edu.co', 
+        :password => '123456' 
+    })
+    user.save!
+end
