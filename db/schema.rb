fec7aa4aacdb9031180fbdc4d63b17734aba440f
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-29 00:30:22 +0000

    Sprint 2

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20170927152456) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "nick"
-    t.string "career"
+    t.string "career", default: false
     t.datetime "date_of_birth"
     t.string "provider"
     t.string "uid"
