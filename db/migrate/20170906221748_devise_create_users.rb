3726fcda820da5da64064cc2c27a27937b0deef2
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-29 00:07:40 +0000

    Sprint 2

--- db/migrate/20170906221748_devise_create_users.rb
+++ db/migrate/20170906221748_devise_create_users.rb
@@ -4,8 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name,               null: false, default: ""
-      
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -37,7 +36,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :name
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
