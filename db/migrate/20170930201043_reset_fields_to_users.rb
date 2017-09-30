acf256699a5bfb2330178d1d9195ada0dc822e45
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-09-30 20:25:13 +0000

    Sprint 2

--- db/migrate/20170930201043_reset_fields_to_users.rb
+++ db/migrate/20170930201043_reset_fields_to_users.rb
@@ -1,9 +1,9 @@
 class ResetFieldsToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :nick, :string
-    add_column :users, :career, :string
-    add_column :users, :date_of_birth, :datetime
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
+    remove_column :users, :nick, :string
+    remove_column :users, :career, :string
+    remove_column :users, :date_of_birth, :datetime
+    remove_column :users, :provider, :string
+    remove_column :users, :uid, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927152456) do
+ActiveRecord::Schema.define(version: 20170930202345) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,14 +26,14 @@ ActiveRecord::Schema.define(version: 20170927152456) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "nick"
-    t.string "career", default: "f"
-    t.datetime "date_of_birth"
-    t.string "provider"
-    t.string "uid"
     t.string "profile_image"
     t.boolean "email_confirmed", default: false
     t.string "confirm_token"
+    t.string "nick"
+    t.string "career"
+    t.datetime "date_of_birth"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
