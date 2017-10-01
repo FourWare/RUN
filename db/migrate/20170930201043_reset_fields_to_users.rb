ddf78a7a792e877da4237fc2690460b148fbedf5
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-10-01 17:15:17 +0000

    Sprint 2

--- db/migrate/20170930201043_reset_fields_to_users.rb
+++
@@ -1,9 +0,0 @@
-class ResetFieldsToUsers < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :users, :nick, :string
-    remove_column :users, :career, :string
-    remove_column :users, :date_of_birth, :datetime
-    remove_column :users, :provider, :string
-    remove_column :users, :uid, :string
-  end
-end
