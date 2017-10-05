3f127c76e7bf48ad4964472cde0c8513fbd12f55
Miguel Villamil <miavillamilca@unal.edu.co>
2017-10-05 20:05:35 +0000

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'home/recuperacion_contrasena' => 'home#recuperacion_contrasena'
   get 'home/registro'
   get '/home/entrar'
+  get '/routes'
   
   root to: 'home#index'
   
