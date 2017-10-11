12064c55f4b5e82718a1fe4f4ccafcc0c90aadff
Miguel Villamil <miavillamilca@unal.edu.co>
2017-10-11 04:17:07 +0000

    adding waypoints support

--- db/migrate/20171004195529_create_routes.rb
+++ db/migrate/20171004195529_create_routes.rb
@@ -7,7 +7,7 @@ class CreateRoutes < ActiveRecord::Migration[5.1]
       t.float :from_lng
       t.float :to_lat
       t.float :to_lng
-      t.column :waypoints, :json
+      t.string :waypoints
       t.datetime :departure
       t.float :cost
       t.timestamps
