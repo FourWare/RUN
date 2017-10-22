b40fd6a09cea44fc5fc8f1982f03b0a0c8bdae1e
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-10-22 05:11:47 +0000

    Cache

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012011441) do
+ActiveRecord::Schema.define(version: 20171018201759) do
 
   create_table "cars", force: :cascade do |t|
     t.string "placa"
