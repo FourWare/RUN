a6c766a053dfa2feb7c83606e80c1242443d3b4a
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-10-25 17:42:42 +0000

    Generación de estadísticas para el administrador de la app

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -19,8 +19,9 @@ class AdminController < ApplicationController
     @carsPerModel = Car.group("modelo").count
     @app = 'RunApp'
     respond_to do |format|
-    format.html {render layout: 'admin_layout' }
-    format.pdf { render template: 'admin/pdf_statistics',javascript_delay: 3000, pdf:'pdf', layout: 'pdf_statistics.html'}
-  end
+      format.html {render layout: 'admin_layout' }
+      format.pdf { render template: 'admin/pdf_statistics',javascript_delay: 3000, pdf:'pdf', layout: 'pdf_statistics.html'}
+    end
+    render :layout => 'admin_layout'
   end
 end
