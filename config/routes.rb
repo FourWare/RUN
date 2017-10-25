2e0bef1b812385c43271e12d39ca7182ea7ef360
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-10-25 04:37:17 +0000

    Corrección de errores en creación de ruta

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get '/home/recuperacion_contrasena' => 'home#recuperacion_contrasena'
   get '/home/registro'
   get '/home/entrar'
+  get '/home/salir'
   
   root to: 'home#index'
   
