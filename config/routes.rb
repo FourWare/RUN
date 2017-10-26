61b06aea8da059a5c859e351859a4dff41ae11f5
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-10-26 05:18:20 +0000

    Organización de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   
   resources :routes, :path => "rutas"
+  resources :cars, :path => "usuario/mis-vehiculos", :path_names => { :new => 'crear-vehiculo', :edit => 'editar' }
+  resources :contacts, only: [:new, :create]
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
+  
+  devise_for :users, :path => 'home', :path_names => { :sign_in => 'entrar', :sign_up => 'registro', :sign_out => 'logout', :edit => 'editar_perfil', :password => 'recuperacion_contrasena', :confirmation => 'verificación',  },
+            :controllers => { omniauth_callbacks: "sessions" }
+
   get '/usuario/updateSpacesAvailable' => 'routes#updateSpacesAvailable'
   get '/usuario/mis-rutas' => 'routes#show_my_routes'
-  
-  resources :cars, :path => "usuario/mis-vehiculos", :path_names => { :new => 'crear-vehiculo', :edit => 'editar' }
-  
   get '/usuario/contactanos' => 'users#create_contactanos'
   get '/usuario/contactanos' => 'users#new_contactanos'
   get '/usuario/editar_perfil' => 'home#editar_perfil'
@@ -19,19 +27,6 @@ Rails.application.routes.draw do
   get '/home/recuperacion_contrasena' => 'home#recuperacion_contrasena'
   get '/home/registro'
   get '/home/entrar'
-
   root to: 'home#index'
-  
-  resources :contacts, only: [:new, :create]
-  resources :users do
-    member do
-      get :confirm_email
-    end
-  end
-  
-  devise_for :users, :path => 'home', :path_names => { :sign_in => 'entrar', :sign_up => 'registro', :sign_out => 'logout', :edit => 'editar_perfil', :password => 'recuperacion_contrasena', :confirmation => 'verificación',  },
-            :controllers => { omniauth_callbacks: "sessions" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 
 end
