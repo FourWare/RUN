c28764e6a9f3e42a9057b01ad9f857bfcd1225fe
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-11-01 17:59:47 +0000

    Implementación del estilo de la calificación de ruta.

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -91,7 +91,6 @@ function restartSearch(){
 }
 
 /* -------------- Funciones para las calificaciones de ruta ---------------- */
-
 function ratingEffect(star, option){
     if(option == "over"){
         if(star == "star1")
@@ -112,56 +111,85 @@ function ratingEffect(star, option){
             $("#star4").css('background-color', "#F3C921");
         }
         else if(star == "star5"){
-            $("#star1").css('background-color', "#F3C921");
-            $("#star2").css('background-color', "#F3C921");
-            $("#star3").css('background-color', "#F3C921");
-            $("#star4").css('background-color', "#F3C921");
-            $("#star5").css('background-color', "#F3C921");
+            $("button[id^='star']").css('background-color', "#F3C921");
         }
     }else if(option == "out"){
-        if(star == "star1")
-            $("#star1").css('background-color', "lightgray");
-        else if(star == "star2"){
-            $("#star1").css('background-color', "lightgray");
-            $("#star2").css('background-color', "lightgray");
-        }
-        else if(star == "star3"){
-            $("#star1").css('background-color', "lightgray");
-            $("#star2").css('background-color', "lightgray");
-            $("#star3").css('background-color', "lightgray");
-        }
-        else if(star == "star4"){
-            $("#star1").css('background-color', "lightgray");
-            $("#star2").css('background-color', "lightgray");
-            $("#star3").css('background-color', "lightgray");
-            $("#star4").css('background-color', "lightgray");
-        }
-        else if(star == "star5"){
-            $("#star1").css('background-color', "lightgray");
-            $("#star2").css('background-color', "lightgray");
-            $("#star3").css('background-color', "lightgray");
-            $("#star4").css('background-color', "lightgray");
-            $("#star5").css('background-color', "lightgray");
-        }
+        if(star == "star1" )
+            $("button[id^='star']").css('background-color', "lightgray");
+        else if(star == "star2")
+            $("button[id^='star']").css('background-color', "lightgray");
+        else if(star == "star3")
+            $("button[id^='star']").css('background-color', "lightgray");
+        else if(star == "star4")
+            $("button[id^='star']").css('background-color', "lightgray");
+        else if(star == "star5")
+            $("button[id^='star']").css('background-color', "lightgray");
     }
 }
 
 function star1Action(){
     $('#myRatingText').html('1 <small>/5</small>');
+    $('#ratingBtn').slideToggle("slow");
+    $('#ratingSaved').slideToggle("slow");
+    $('#countStar1').text(' ' + (parseInt($('#countStar1').text()) + 1 ));
+    updateBars();
 }
 
 function star2Action(){
     $('#myRatingText').html('2 <small>/5</small>');
+    $('#ratingBtn').slideToggle("slow");
+    $('#ratingSaved').slideToggle("slow");
+    $('#countStar2').text(' ' + (parseInt($('#countStar2').text()) + 1 ));
+    updateBars();
 }
 
 function star3Action(){
     $('#myRatingText').html('3 <small>/5</small>');
+    $('#ratingBtn').slideToggle("slow");
+    $('#ratingSaved').slideToggle("slow");
+    $('#countStar3').text(' ' + (parseInt($('#countStar3').text()) + 1 ));
+    updateBars();
 }
 
 function star4Action(){
     $('#myRatingText').html('4 <small>/5</small>');
+    $('#ratingBtn').slideToggle("slow");
+    $('#ratingSaved').slideToggle("slow");
+    $('#countStar4').text(' ' + (parseInt($('#countStar4').text()) + 1 ));
+    updateBars();
 }
 
 function star5Action(){
     $('#myRatingText').html('5 <small>/5</small>');
+    $('#ratingBtn').slideToggle("slow");
+    $('#ratingSaved').slideToggle("slow");
+    $('#countStar5').text(' ' + (parseInt($('#countStar5').text()) + 1 ));
+    updateBars();
+}
+
+function addUserRating(){
+    $('#countUsersRating').text(' ' + (parseInt($('#countUsersRating').text()) + 1));
+}
+
+function removeUserRating(){
+    $('#countUsersRating').text(' ' + (parseInt($('#countUsersRating').text()) - 1));
+}
+
+function updateBars(){
+    $('#barStar1').css('width', $('#countStar1').text()*100/(parseInt($('#countUsersRating').text())) + '%' );
+    $('#barStar2').css('width', $('#countStar2').text()*100/(parseInt($('#countUsersRating').text())) + '%' );
+    $('#barStar3').css('width', $('#countStar3').text()*100/(parseInt($('#countUsersRating').text())) + '%' );
+    $('#barStar4').css('width', $('#countStar4').text()*100/(parseInt($('#countUsersRating').text())) + '%' );
+    $('#barStar5').css('width', $('#countStar5').text()*100/(parseInt($('#countUsersRating').text())) + '%' );
+}
+
+function ratingCancel() {
+    $('#ratingBtn').slideToggle("slow");
+    $('#ratingSaved').slideToggle("slow");
+    $("button[id^='star']").css('background-color', "lightgray");
+    var myRat = $('#myRatingText').text()[0];
+    $('#countStar' + myRat).text(' ' + (parseInt($('#countStar' + myRat).text()) - 1 ));
+    updateBars();
+    removeUserRating();
+    $('#myRatingText').html('- <small>/5</small>');
 }
--- db/migrate/20171028210345_add_rating_to_route.rb
+++
@@ -1,5 +0,0 @@
-class AddRatingToRoute < ActiveRecord::Migration[5.1]
-  def change
-    add_column :routes, :rating, :float
-  end
-end
