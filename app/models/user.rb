7cfe52c8d30daceb0b8795e9c88d4634439438f7
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-11-12 20:16:28 +0000

    Login con Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
 
   validates_format_of :email, with: /\@unal.edu.co/, message: 'debe ser de la Universidad Nacional de Colombia.'
   validates_uniqueness_of :nick
@@ -17,26 +17,48 @@ class User < ApplicationRecord
     UserMailer.welcome_email(self).deliver_now
   end
   
+  #def self.new_with_session(params, session)
+  #  super.tap do |user|
+  #    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+  #      user.email = data["email"] if user.email.blank?
+  #    end
+  #  end
+  #end
+  
+  #def self.from_omniauth(auth)
+  #  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #    user.email = auth.info.email
+  #    user.password = Devise.friendly_token[0,20]
+  #    user.name = auth.info.name   # assuming the user model has a name
+  #    user.nick = auth.info.nick
+  #    user.date_of_birth =  Date.strptime(auth.extra.raw_info.birthday, '%m/%d/%Y')
+  #    user.profile_image = auth.info.image
+  #    # If you are using confirmable and the provider(s) you use validate emails, 
+  #    # uncomment the line below to skip the confirmation emails.
+  #    # user.skip_confirmation!
+  #  end
+  #end
+
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      if data = session["devise.google_data"] && session["devise.google_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
     end
   end
-  
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.nick = auth.info.nick
-      user.date_of_birth =  Date.strptime(auth.extra.raw_info.birthday, '%m/%d/%Y')
-      user.profile_image = auth.info.image
-      # If you are using confirmable and the provider(s) you use validate emails, 
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
-    end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    # unless user
+    #     user = User.create(name: data['name'],
+    #        email: data['email'],
+    #        password: Devise.friendly_token[0,20]
+    #     )
+    # end
+    user
   end
   
   private
