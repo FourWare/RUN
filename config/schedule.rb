06292e091e101d5a563d6743385a575fce784d14
Miguel Villamil <miavillamilca@unal.edu.co>
2017-11-19 10:58:03 +0000

    changing settings

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
-set :environment, "development"
-set :output, "#{path}/log/cron_log.log"
+#set :environment, "development"
+set :output, "/log/cron_log.log"
 env :PATH, ENV['PATH']
 
 every 1.day :at => '3 AM' do
