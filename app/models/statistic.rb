c5194306fc2940b82be3e38336294c91bf15cac4
Miguel Villamil <miavillamilca@unal.edu.co>
2017-11-19 10:52:47 +0000

    adding statistics

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,4 +1,24 @@
 class Statistic < ApplicationRecord
     
-  
+    
+    def self.routesAllDays()
+        Statistic.pluck(:routes_oer_day)
+    end
+    
+    def self.usersAllDays()
+        Statistic.pluck(:users_created_per_day)
+    end
+    
+    def self.carsAllDays()
+        Statistic.pluck(:cars_per_day)
+    end
+    
+    def self.bikesAllDays()
+        Statistic.pluck(:bikes_per_day)
+    end
+    
+    def self.usersInAllRoutes()
+        Statistic.pluck(:users_in_routes_per_day)
+    end
+    
 end
